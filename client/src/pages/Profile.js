e086abed277486d9d5b7e61cdcf2672e229c72f5
DomenicoBarb <domenicobarberi585@gmail.com>
2023-05-31 20:32:59 -0400

    added ANT style UI, modified pages to use styling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Jumbotron, Container } from 'react-bootstrap';
-// temporary es lint disable until implemented
-// eslint-disable-next-line no-unused-vars
-import Auth from '../utils/auth';
+import { Typography, Row, Col } from 'antd';
 import { useQuery } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
+
+const { Title } = Typography;
 
 const Profile = () => {
   const { loading, data } = useQuery(GET_ME);
@@ -16,17 +15,19 @@ const Profile = () => {
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
-        <Container>
-          <h1>Profile Page</h1>
-        </Container>
-      </Jumbotron>
-      <Container>
-        <h2>Username: {userData.username}</h2>
-        <h2>Email: {userData.email}</h2>
-        <h2>High Score: {userData.highScore}</h2>
-        <h2>Last Score: {userData.lastScore}</h2>
-      </Container>
+      <Row justify="center" align="middle" style={{ height: '100px', background: '#000', color: '#fff' }}>
+        <Col>
+          <Title level={1} style={{ color: '#fff' }}>Profile Page</Title>
+        </Col>
+      </Row>
+      <Row justify="center" style={{ marginTop: '20px' }}>
+        <Col>
+          <Title level={2}>Username: {userData.username}</Title>
+          <Title level={2}>Email: {userData.email}</Title>
+          <Title level={2}>High Score: {userData.highScore}</Title>
+          <Title level={2}>Last Score: {userData.lastScore}</Title>
+        </Col>
+      </Row>
     </>
   );
 };
