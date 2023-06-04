9ec28b07e99d6f2fea6fd1ccb75b699e87a0b565
DomenicoBarb <domenicobarberi585@gmail.com>
2023-06-04 19:55:27 -0400

    made each page header background colour match ant designs darkmode"

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -8,7 +8,7 @@ const { Title } = Typography;
 const GamePage = () => {
   return (
     <Layout>
-      <Row justify="center" align="middle" style={{ height: '100px', background: '#000', color: '#fff' }}>
+      <Row justify="center" align="middle" style={{ height: '100px', background: '#001529', color: '#fff' }}>
         <Col>
           <Title level={1} style={{ color: '#fff', textAlign: 'center' }}>Match the cards in as few turns as possible!</Title>
         </Col>
