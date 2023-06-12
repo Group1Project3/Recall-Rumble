47da21bf8a2cc49aac8a3c539a9a6cc6e65defcc
DomenicoBarb <domenicobarberi585@gmail.com>
2023-06-12 15:03:05 -0400

    adding logo to intro page

--- client/src/pages/Intro.js
+++ client/src/pages/Intro.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Typography, Row, Col, Button, Modal, Tabs } from "antd";
 import SignUpForm from "../components/SignupForm";
 import LoginForm from "../components/LoginForm";
-// import logo from "../images/logo.png";
 import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
 const { Title } = Typography;
@@ -13,34 +12,29 @@ const Intro = () => {
 
   return (
     <>
-      <Row justify="center" className='bg' align="middle" style={{ height: '100vh', color: '#fff', textAlign: 'center' }}>
+      <Row justify="center" className="bg" align="middle" style={{ height: "100vh", color: "#fff", textAlign: "center" }}>
         <Col>
-          <Title level={1} className='pageheader' style={{ color: '#fff', textAlign: 'center', width: '80vw' }}>
+          <img src="../assets/images/logo.png" alt="Logo" style={{ width: "50%", maxWidth: "300px", margin: "0 auto" }} />
+          <Title level={1} className="pageheader" style={{ color: "#fff", textAlign: "center", width: "80vw" }}>
             {Auth.loggedIn() ? "Now that you have an account," : "Hey there, welcome to Recall Rumble!"}
           </Title>
-          <Title level={1} className='pageheader' style={{ color: '#fff', textAlign: 'center', width: '80vw' }}>
+          <Title level={1} className="pageheader" style={{ color: "#fff", textAlign: "center", width: "80vw" }}>
             {Auth.loggedIn() ? "Press the button below to play!" : "Click the button below to login or to make an account!"}
           </Title>
           {Auth.loggedIn() ? (
             <Link to="/Game">
-              <Button className='pageheader' size="large" type="primary" style={{ justifyContent: 'center', marginTop: '1rem' }}>
+              <Button className="pageheader" size="large" type="primary" style={{ justifyContent: "center", marginTop: "1rem" }}>
                 Recall Rumble Time!
               </Button>
             </Link>
           ) : (
-            <Button className='pageheader' size="large" type="primary" onClick={() => setShowModal(true)} style={{ justifyContent: 'center', marginTop: '1rem' }}>
+            <Button className="pageheader" size="large" type="primary" onClick={() => setShowModal(true)} style={{ justifyContent: "center", marginTop: "1rem" }}>
               Login / Sign Up
             </Button>
-            
           )}
         </Col>
       </Row>
-      <Modal
-        centered
-        open={showModal}
-        onCancel={() => setShowModal(false)}
-        footer={null}
-      >
+      <Modal centered open={showModal} onCancel={() => setShowModal(false)} footer={null}>
         <Tabs defaultActiveKey="login" centered>
           <TabPane tab="Login" key="login">
             <LoginForm handleModalClose={() => setShowModal(false)} />
